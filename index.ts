5de549f37cd31a7ce0dc8c4da12d0dd236e78bcd
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-09-27 16:55:33 +0700

    2.2. Kết nối Database

--- index.ts
+++ index.ts
@@ -1,11 +1,27 @@
 import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import { connectDatabase } from "./config/database";
+import Article from "./models/article.model";
 const app: Express = express();
 const port: number = 3000;
-
+connectDatabase();
 //Rest API
-app.get("/articles", (req: Request, res: Response) => {
+// app.get("/articles", async (req: Request, res: Response) => {
+//   const articles = await Article.find({
+//     deleted: false
+//   })
+//   console.log(articles);
+//   res.json({
+//     articles: articles
+//   });
+// });
+app.get("/articles", async (req: Request, res: Response) => {
+  const articles = await Article.find({
+    deleted: false
+  });
   res.json({
-    articles: []
+    articles: articles
   });
 });
 app.listen(port, () => {
