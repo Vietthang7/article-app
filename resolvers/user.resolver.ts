eb37299ce61b43955748703d87761b0c9d3ee8d3
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-09-30 18:48:32 +0700

    2. Tính năng Đăng ký

---
+++ resolvers/user.resolver.ts
@@ -0,0 +1,39 @@
+import { register } from "module";
+import { generateRandomString } from "../helpers/generate.helper";
+import User from "../models/user.model";
+import md5 from "md5";
+import { emit } from "process";
+export const resolversUser = {
+  Mutation: {
+    register: async (_, args) => {
+      const { user } = args;
+      const existUser = await User.findOne({
+        email: user.email,
+        deleted: false
+      });
+      if (existUser) {
+        return {
+          code: 400,
+          message: "Email đã tồn tại"
+        };
+      }
+      const token = generateRandomString(30);
+      const dataUser = {
+        fullName: user.fullName,
+        email: user.email,
+        password: md5(user.password),
+        token: token
+      };
+      const newUser = new User(dataUser);
+      await newUser.save();
+      return {
+        id: newUser.id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        token: newUser.token,
+        code: 200,
+        message: "Đăng ký thành công!"
+      };
+    }
+  }
+}
